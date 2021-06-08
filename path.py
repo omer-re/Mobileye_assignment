c6d37e7f44aab08c56b59bcb9e75fe15cb875c01
Omer Re <39154767+omer-re@users.noreply.github.com>
2021-06-08 18:22:06 +0300

    day 2 4

--- path.py
+++ path.py
@@ -1,30 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+
 # linked list of points
 class path:
-    #list of points
-    path_points= [(2, 8), (1, 3), (0, 2), (0, 0), (6, 1), (9, 3), (8, 4), (7, 4), (6, 4)]
-    points_counter=9
-    points_done=0
+    # list of points
+    path_points = [(2, 8), (1, 3), (0, 2), (0, 0), (6, 1), (9, 3), (8, 4), (7, 4), (6, 4)]
+    points_counter = 9
+    points_done = 0
 
     def get_path_array(self):
-        return  np.array(self.path_points)
+        return np.array(self.path_points)
 
-
-    def path_add_point(self, x_val,y_val):
-        tup= (x_val,y_val)
+    def path_add_point(self, x_val, y_val):
+        tup = (x_val, y_val)
         self.path.append(tup)
-        self.points_counter+=1
-
+        self.points_counter += 1
 
     def plot_path(self):
         data = np.array(self.path_points)
         plt.plot(*data.T)
-        plt.plot(*data.T,'ro')
-        plt.plot(*data.T,'b-')
+        plt.plot(*data.T, 'ro')
+        plt.plot(*data.T, 'b-')
         plt.show()
-
-
 
 # path1=path()
 # path1.plot_path()
--- simulation.py
+++ simulation.py
@@ -5,35 +5,32 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-
-
 # Simulation parameters
 global k
 global Lfc
 global Kp
 global dt
-k= 0.1 # look-ahead gain
-Lfc= 1.0 # look ahead distance
-Kp = 1 # speed gain (proportional), for us it's const
-dt = 0.002 # in secs = 200 milli sec
+k = 0.1  # look-ahead gain
+Lfc = 1.0  # look ahead distance
+Kp = 1  # speed gain (proportional), for us it's const
+dt = 0.002  # in secs = 200 milli sec
 
-speed_kmh= 10.0
-speed_msec= speed_kmh/3.6
-
+speed_kmh = 10.0
+speed_msec = speed_kmh / 3.6
 
 # initialize car
-car1=car_state.Car(0, 0, 0, 0)
-_path=path.path()
+car1 = car_state.Car(0, 0, 0, 0)
+_path = path.path()
 
-frames= len(_path.path_points)
+frames = len(_path.path_points)
 
-x,y,yaw,v=(car1.x,car1.y,car1.yaw,car1.v)
-t=0.0
-target_point=car_state.find_next_point(car1,_path, _path.path_points[0][0], _path.path_points[0][1], 0)
+x, y, yaw, v = (car1.x, car1.y, car1.yaw, car1.v)
+t = 0.0
+target_point = car_state.find_next_point(car1, _path, _path.path_points[0][0], _path.path_points[0][1], 0)
 
-for counter,t_seg in enumerate(frames):
+for counter, t_seg in enumerate(frames):
     ai = Kp * (speed_msec - car1.v)  # PID controller
-    delta_i, target_point= car1.pure_pursuit_control(car1, _path, car1.x, car1.y, counter)
+    delta_i, target_point = car1.pure_pursuit_control(car1, _path, car1.x, car1.y, counter)
     car1 = car_state.update(car1, ai, delta_i)
 
     plt.cla()
